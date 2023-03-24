080db55839e3493b5b2ae80c28ebeab693a3821a
unknown <ali.mnp.uni@gmail.com>
2023-03-25 00:25:46 +0430

    .

---
+++ Project 1/Code/main.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def calculate_population(): # This function calculates the total population of the neighborhood
+    with open('blocks_population.txt') as f:
+        lines = f.read().splitlines()
+
+    neighborPopulation = []
+    for i in lines:
+        neighborPopulation.extend(i.split(','))
+
+    result = np.array(neighborPopulation, dtype=int)
+    totalPopulation = np.sum(result)
+    return totalPopulation
+
